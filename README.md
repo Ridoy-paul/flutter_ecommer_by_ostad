388b6a6189efeff7c2e79a46406deb0ae9558b58
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-11 21:22:06 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_ecommer_apps
+ 
---
+++ android/app/src/main/kotlin/com/example/flutter_ecommer_by_ostad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ecommer_by_ostad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
