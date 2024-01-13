9043d74701eb908a837609e431ec53c8cb530da5
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-13 21:49:58 +0600

    up

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShoppersMart extends StatelessWidget {
+  const ShoppersMart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
