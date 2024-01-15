2015daced107b5eaab5e806a7b33eb65e5aabf70
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-16 00:32:20 +0600

    up

--- lib/presentation/ui/screens/verify_email_screen.dart
+++ lib/presentation/ui/screens/verify_email_screen.dart
@@ -14,34 +14,36 @@ class _VerifyEmailScreenState extends State<VerifyEmailScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: Get.height * .1,),
-                const AppLogoWidget(),
-                const SizedBox(height: 16,),
-                Text("Welcome back", style: Theme.of(context).textTheme.titleLarge,),
-                const SizedBox(height: 5,),
-                Text("Please Enter Your Email Address", style: Theme.of(context).textTheme.bodySmall,),
-                const SizedBox(height: 16,),
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Email',
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: Get.height * .1,),
+                  const AppLogoWidget(),
+                  const SizedBox(height: 16,),
+                  Text("Welcome back", style: Theme.of(context).textTheme.titleLarge,),
+                  const SizedBox(height: 5,),
+                  Text("Please Enter Your Email Address", style: Theme.of(context).textTheme.bodySmall,),
+                  const SizedBox(height: 16,),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                    ),
                   ),
-                ),
-                const SizedBox(height: 16,),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-
-                    },
-                    child: const Text("Next"),
-                  ),
-                )
-              ],
+                  const SizedBox(height: 16,),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+          
+                      },
+                      child: const Text("Next"),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
