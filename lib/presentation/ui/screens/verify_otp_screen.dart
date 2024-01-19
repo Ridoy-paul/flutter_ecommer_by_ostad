c6aef1877730bdd0e4f5592f3e6f722ed7005199
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-19 22:53:34 +0600

    pin code verification update

--- lib/presentation/ui/screens/verify_otp_screen.dart
+++ lib/presentation/ui/screens/verify_otp_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommer_by_ostad/presentation/ui/utility/app_colors.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import '../widgets/app_logo.dart';
 import 'package:get/get.dart';
 
@@ -27,10 +29,38 @@ class _VerifyOTPScreenState extends State<VerifyOTPScreen> {
                   const SizedBox(height: 5,),
                   Text("A 4 digit code has been sent", style: Theme.of(context).textTheme.bodyLarge,),
                   const SizedBox(height: 16,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Email',
+                  PinCodeTextField(
+                    //controller: ,
+                    //validator: (value) => inputValidate(value, "Enter 6 digit verification code!"),
+                    length: 4,
+                    obscureText: false,
+                    animationType: AnimationType.fade,
+                    pinTheme: PinTheme(
+                      shape: PinCodeFieldShape.box,
+                      borderRadius: BorderRadius.circular(5),
+                      fieldHeight: 50,
+                      fieldWidth: 40,
+                      activeFillColor: Colors.white,
+                      selectedFillColor: AppColors.primaryColor,
+                      inactiveFillColor: AppColors.primaryColor,
                     ),
+                    animationDuration: const Duration(milliseconds: 300),
+                    enableActiveFill: true,
+                    //errorAnimationController: errorController,
+
+                    onCompleted: (v) {
+                      //print("Completed");
+                    },
+                    onChanged: (value) {
+
+                    },
+                    beforeTextPaste: (text) {
+                      //print("Allowing to paste $text");
+                      //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                      //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                      return true;
+                    },
+                    appContext: context,
                   ),
                   const SizedBox(height: 16,),
                   SizedBox(
