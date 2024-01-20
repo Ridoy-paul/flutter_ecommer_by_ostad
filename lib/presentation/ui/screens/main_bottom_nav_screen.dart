47e191ceef34175485ca9568e0b314131bb20e73
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-20 12:44:51 +0600

    updated

--- lib/presentation/ui/screens/main_bottom_nav_screen.dart
+++ lib/presentation/ui/screens/main_bottom_nav_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommer_by_ostad/presentation/ui/utility/app_colors.dart';
 
 class MainBottomNavScreen extends StatefulWidget {
   const MainBottomNavScreen({super.key});
@@ -10,6 +11,18 @@ class MainBottomNavScreen extends StatefulWidget {
 class _MainBottomNavScreenState extends State<MainBottomNavScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: AppColors.primaryColor,
+        unselectedItemColor: AppColors.lightGray,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.dashboard), label: "Categories"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: "Wish"),
+        ],
+      ),
+    );
   }
 }
