394b43881ceba5dfce9a4cd5fb2ef20d2744f32e
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-20 19:32:21 +0600

    carosel updated

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommer_by_ostad/presentation/ui/utility/app_colors.dart';
 import '../utility/assets_path.dart';
@@ -24,7 +25,40 @@ class _HomeScreenState extends State<HomeScreen> {
                 const SizedBox(height: 8,),
                 searchTextFormField,
                 const SizedBox(height: 16,),
-                
+
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 200,
+                    aspectRatio: 16/2,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    //onPageChanged: callbackFunction,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                  items: [1,2,3,4,5].map((i) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(
+                                color: Colors.amber
+                            ),
+                            child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                        );
+                      },
+                    );
+                  }).toList(),
+                )
+
 
               ],
             ),
