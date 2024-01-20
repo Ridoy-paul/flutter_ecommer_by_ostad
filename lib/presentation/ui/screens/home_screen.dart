d9ec3dfbf9ced2aa75a88f347ede3167cb323d23
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-20 16:21:30 +0600

    nav updated

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -13,38 +13,43 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(AssetsPath.logoNav),
-        actions: [
-          CircleIconButton(
-            onTap: () {
-              print("hello");
-            },
-            iconData: Icons.person_outline,
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-          CircleIconButton(
-            onTap: () {
-              print("hello");
-            },
-            iconData: Icons.call_outlined,
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-          CircleIconButton(
-            onTap: () {
-              print("hello");
-            },
-            iconData: Icons.notification_important_outlined,
-          ),
-          const SizedBox(
-            width: 8,
-          ),
-        ],
-      ),
+      appBar: appBar,
     );
   }
+
+  AppBar get appBar {
+    return AppBar(
+      title: Image.asset(AssetsPath.logoNav),
+      actions: [
+        CircleIconButton(
+          onTap: () {
+            print("hello");
+          },
+          iconData: Icons.person_outline,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        CircleIconButton(
+          onTap: () {
+            print("hello");
+          },
+          iconData: Icons.call_outlined,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+        CircleIconButton(
+          onTap: () {
+            print("hello");
+          },
+          iconData: Icons.notification_important_outlined,
+        ),
+        const SizedBox(
+          width: 8,
+        ),
+      ],
+    );
+  }
+
 }
