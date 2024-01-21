a330fd20215b86cc83be5ca9c50a6307209629f6
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-21 22:08:31 +0600

    category page running

--- lib/presentation/ui/screens/category_screen.dart
+++ lib/presentation/ui/screens/category_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommer_by_ostad/presentation/ui/widgets/category_item_widget.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -10,6 +11,30 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+        title: Text(
+          "Categories",
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+      body: SafeArea(
+        child: GridView.builder(
+            itemCount: 20,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              childAspectRatio: 1,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8
+            ),
+            itemBuilder: (context, index) {
+              return FittedBox(child: CategoryItemWidget(categoryName: "Fish"));
+            }),
+      ),
+    );
   }
 }
