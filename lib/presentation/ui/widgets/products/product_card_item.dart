c741d1d338758def8c7054eb1f0ba879feb568af
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-24 20:30:16 +0600

    updated

--- lib/presentation/ui/widgets/products/product_card_item.dart
+++ lib/presentation/ui/widgets/products/product_card_item.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommer_by_ostad/presentation/ui/screens/product_details_screen.dart';
 import 'package:get/get.dart';
 import '../../utility/app_colors.dart';
 
@@ -10,81 +11,87 @@ class ProductCardItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: Get.width * .35,
-     // height: Get.height * .24,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Column(
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(16),
-                topRight: Radius.circular(16),
+    return InkWell(
+      onTap: () {
+        Get.to(() => ProductDetailsScreen());
+      },
+      borderRadius: BorderRadius.circular(16),
+      child: SizedBox(
+        width: Get.width * .35,
+       // height: Get.height * .24,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16),
+                  topRight: Radius.circular(16),
+                ),
+                child: Image.asset(
+                  'assets/images/shoe.png',
+                  width: Get.width * .3,
+                  height: 120,
+                  fit: BoxFit.cover,
+                ),
               ),
-              child: Image.asset(
-                'assets/images/shoe.png',
-                width: Get.width * .3,
-                height: 120,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "New year special shoe 30",
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
-                  ),
-                  const SizedBox(height: 4,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "\$120",
-                        style: TextStyle(
-                          fontSize: 12,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "New year special shoe 30",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(height: 4,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "\$120",
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: AppColors.primaryColor,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        //const SizedBox(width: 5,),
+                        Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.center,
+                          children: [
+                            Icon(Icons.star, size: 12, color: Colors.amber,),
+                            Text(
+                              "4.8",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          ],
+                        ),
+                        Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
                           color: AppColors.primaryColor,
-                          fontWeight: FontWeight.w600,
+                          child: Padding(
+                            padding: const EdgeInsets.all(4.0),
+                            child: Icon(Icons.favorite_outline, size: 12, color: Colors.white,),
+                          ),
                         ),
-                      ),
-                      //const SizedBox(width: 5,),
-                      Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.center,
-                        children: [
-                          Icon(Icons.star, size: 12, color: Colors.amber,),
-                          Text(
-                            "4.8",
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          )
-                        ],
-                      ),
-                      Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        color: AppColors.primaryColor,
-                        child: Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Icon(Icons.favorite_outline, size: 12, color: Colors.white,),
-                        ),
-                      ),
-                      const SizedBox(width: 5,),
-                    ],
-                  )
-                ],
+                        const SizedBox(width: 5,),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
