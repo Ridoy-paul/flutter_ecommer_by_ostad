d0c462f90b7d04f96018ceec5b132ce724fd2ae7
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-26 20:36:08 +0600

    model updated

--- lib/presentation/state_holders/send_email_otp_controller.dart
+++ lib/presentation/state_holders/send_email_otp_controller.dart
@@ -5,14 +5,25 @@ import 'package:get/get.dart';
 
 class SendEmailOTPController extends GetxController {
   bool _inProgress = false;
+  String _message = '';
+  bool _isSuccess = true;
 
   bool get inProgressStatus => _inProgress;
+  String get message => _message;
+  bool get isSuccess => _isSuccess;
 
   Future<bool> sendEmailOTP(String email) async {
     _inProgress = true;
     update();
 
     final ResponseData response = await NetworkCaller().getRequest(Urls.userLoginEmailVerification(email));
+
+    if(response.isSuccess) {
+
+    }
+    else {
+
+    }
 
 
 
