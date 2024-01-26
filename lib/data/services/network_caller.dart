fd324d613526e8acbb27ce5a7bdaba46aadaf495
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-26 13:22:57 +0600

    network caller updated

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -1,7 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart';
 import 'response_data.dart';
 
 class NetworkCaller  {
   Future<ResponseData> getRequest(String url) async {
+    final Response response = await get(Uri.parse(url));
+    if(response.statusCode == 200) {
+      final decodedResponse = jsonDecode(response.body);
+      if(decodedResponse['msg'] == 'success') {
+        return ResponseData(isSuccess: true, statusCode: response.statusCode, responseData: decodedResponse);
+      }
+      else {
+        return ResponseData(isSuccess: false, statusCode: response.statusCode, responseData: decodedResponse);
+      }
 
+    }
   }
 }
