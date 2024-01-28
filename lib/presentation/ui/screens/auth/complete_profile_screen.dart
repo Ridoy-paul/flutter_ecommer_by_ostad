42bf1ef94590d7db81d9f946167ba7508d76ee0a
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-01-29 00:14:23 +0600

    complete profile updated

--- lib/presentation/ui/screens/auth/complete_profile_screen.dart
+++ lib/presentation/ui/screens/auth/complete_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommer_by_ostad/data/utility/helpers.dart';
 import '../main_bottom_nav_screen.dart';
 import '../../widgets/app_logo.dart';
 import 'package:get/get.dart';
@@ -11,68 +12,88 @@ class CompleteProfileScreen extends StatefulWidget {
 }
 
 class _CompleteProfileScreenState extends State<CompleteProfileScreen> {
+
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final TextEditingController _mobileNameTEController = TextEditingController();
+  final TextEditingController _cityNameTEController = TextEditingController();
+  final TextEditingController _shippingAddressNameTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Center(
-              child: Column(
-                children: [
-                  SizedBox(height: Get.height * .02,),
-                  const AppLogoWidget(),
-                  const SizedBox(height: 16,),
-                  Text("Complete Profile", style: Theme.of(context).textTheme.titleLarge,),
-                  const SizedBox(height: 5,),
-                  Text("Get started with us with your details", style: Theme.of(context).textTheme.bodyLarge,),
-                  const SizedBox(height: 16,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'First Name',
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Center(
+                child: Column(
+                  children: [
+                    SizedBox(height: Get.height * .02,),
+                    const AppLogoWidget(),
+                    const SizedBox(height: 16,),
+                    Text("Complete Profile", style: Theme.of(context).textTheme.titleLarge,),
+                    const SizedBox(height: 5,),
+                    Text("Get started with us with your details", style: Theme.of(context).textTheme.bodyLarge,),
+                    const SizedBox(height: 16,),
+                    TextFormField(
+                      controller: _firstNameTEController,
+                      validator: (value) => inputValidate(value, "Enter First Name"),
+                      decoration: const InputDecoration(
+                        hintText: 'First Name',
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 8,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Last Name',
+                    const SizedBox(height: 8,),
+                    TextFormField(
+                      controller: _lastNameTEController,
+                      validator: (value) => inputValidate(value, "Enter Last Name"),
+                      decoration: const InputDecoration(
+                        hintText: 'Last Name',
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 8,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Mobile',
+                    const SizedBox(height: 8,),
+                    TextFormField(
+                      controller: _mobileNameTEController,
+                      validator: (value) => inputValidate(value, "Enter Mobile Number"),
+                      decoration: const InputDecoration(
+                        hintText: 'Mobile',
+                      ),
+                      keyboardType: TextInputType.number,
                     ),
-                    keyboardType: TextInputType.number,
-                  ),
-                  const SizedBox(height: 8,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'City',
+                    const SizedBox(height: 8,),
+                    TextFormField(
+                      controller: _cityNameTEController,
+                      validator: (value) => inputValidate(value, "Enter Your City Name"),
+                      decoration: const InputDecoration(
+                        hintText: 'City',
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 8,),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Shipping Address',
+                    const SizedBox(height: 8,),
+                    TextFormField(
+                      controller: _shippingAddressNameTEController,
+                      validator: (value) => inputValidate(value, "Enter Shipping Address"),
+                      decoration: const InputDecoration(
+                        hintText: 'Shipping Address',
+                      ),
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 3,
                     ),
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 3,
-                  ),
-                  const SizedBox(height: 8,),
+                    const SizedBox(height: 8,),
 
-
-                  SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Get.offAll(() => const MainBottomNavScreen());
-                      },
-                      child: const Text("Next"),
-                    ),
-                  )
-                ],
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Get.offAll(() => const MainBottomNavScreen());
+                        },
+                        child: const Text("Next"),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -80,4 +101,16 @@ class _CompleteProfileScreenState extends State<CompleteProfileScreen> {
       ),
     );
   }
+
+
+  @override
+  void dispose() {
+    _firstNameTEController.dispose();
+    _lastNameTEController.dispose();
+    _mobileNameTEController.dispose();
+    _cityNameTEController.dispose();
+    _shippingAddressNameTEController.dispose();
+    super.dispose();
+  }
+
 }
