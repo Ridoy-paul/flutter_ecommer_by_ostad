d21d2bcf97d6ff2e3f347b9c37f1f278c049ca3e
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-02-03 21:03:45 +0600

    updated

---
+++ lib/data/utility/country_cities_and_state_utility.dart
@@ -0,0 +1,31 @@
+class CountriesCitiesAndStates {
+  final Map<String, Map<String, List<String>>> countriesCitiesAndStates = {
+    'Bangladesh': {
+      'Dhaka': ['Dhaka'],
+      'Chittagong': ['Chittagong', 'Feni', 'Cox\'s Bazar', 'Comilla', 'Chandpur', 'Brahmanbaria', 'Rangamati', 'Khagrachhari', 'Bandarban', 'Noakhali', 'Lakshmipur'],
+      'Khulna': ['Khulna', 'Jessore', 'Satkhira', 'Bagerhat', 'Chuadanga', 'Kushtia', 'Magura', 'Meherpur', 'Narail', 'Jhenaidah'],
+      'Rajshahi': ['Rajshahi', 'Bogra', 'Pabna', 'Naogaon', 'Joypurhat', 'Sirajganj', 'Chapai Nawabganj'],
+      'Barisal': ['Barisal', 'Bhola', 'Jhalokati', 'Patuakhali', 'Pirojpur'],
+      'Sylhet': ['Sylhet', 'Moulvibazar', 'Habiganj', 'Sunamganj'],
+      'Rangpur': ['Rangpur', 'Dinajpur', 'Gaibandha', 'Kurigram', 'Lalmonirhat', 'Nilphamari', 'Panchagarh', 'Thakurgaon'],
+      'Mymensingh': ['Mymensingh', 'Netrokona', 'Jamalpur', 'Sherpur']
+    },
+    'USA': {
+      'California': ['Los Angeles', 'San Francisco', 'San Diego', 'San Jose', 'Sacramento'],
+      'New York': ['New York City', 'Buffalo', 'Rochester', 'Syracuse', 'Albany'],
+      'Texas': ['Houston', 'Dallas', 'Austin', 'San Antonio', 'Fort Worth'],
+      'Florida': ['Miami', 'Orlando', 'Tampa', 'Jacksonville', 'Fort Lauderdale'],
+    },
+    'India': {
+      'Maharashtra': ['Mumbai', 'Pune', 'Nagpur', 'Nashik', 'Aurangabad'],
+      'Delhi': ['New Delhi'],
+      'Karnataka': ['Bangalore', 'Mysore', 'Hubli', 'Mangalore'],
+      'Tamil Nadu': ['Chennai', 'Coimbatore', 'Madurai', 'Tiruchirappalli'],
+    }
+    // Add more countries as needed
+  };
+
+  //final List<String> usaCaliforniaCities = countriesCitiesAndStates['USA']['California'] ?? [];
+
+
+}
