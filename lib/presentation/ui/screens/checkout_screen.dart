91c57def05e19b2b7de8ac756246f1f0eac07119
Ridoy Paul <cse.ridoypaul@gmail.com>
2024-02-17 23:18:55 +0600

    updated

--- lib/presentation/ui/screens/checkout_screen.dart
+++ lib/presentation/ui/screens/checkout_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommer_by_ostad/presentation/ui/screens/payment_webview_screen.dart';
+import 'payment_webview_screen.dart';
 import '../../state_holders/invoice/create_invoice_controller.dart';
 import '../../../data/utility/helpers.dart';
 import '../utility/app_colors.dart';
